bc0df9ca6e1da0b3cf2c030a32122ae32550496c
teraomarika <marika.terao.moapp@gmail.com>
2020-04-14 19:01:20 +0900

    完成！！

--- app/src/main/java/jp/techacademy/marika/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/marika/calcapp/MainActivity.kt
@@ -30,70 +30,59 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
-        var value1: Double? = edit1.text.toString().toDouble()
-        var value2: Double? = edit2.text.toString().toDouble()
+        var value1: Double? = null
+        var value2: Double? = null
 
+        try {
+            value1 = edit1.text.toString().toDouble()
+            value2 = edit2.text.toString().toDouble()
 
-
-
-
-        if (v != null) {
-            if (v.id == R.id.button_plus) {
-                val intent = Intent(this, Subapp::class.java)
-
-                intent.putExtra("VALUE1", value1)
-                intent.putExtra("VALUE2", value2)
-                startActivity(intent)
-
-            } else if (v.id == R.id.button_minus) {
-                val intent = Intent(this, minus::class.java)
-                intent.putExtra("VALUE3", value1)
-                intent.putExtra("VALUE4", value2)
-                startActivity(intent)
-
-            } else if (v.id == R.id.button_double) {
-                val intent = Intent(this, double1::class.java)
-                intent.putExtra("VALUE5", value1)
-                intent.putExtra("VALUE6", value2)
-                startActivity(intent)
-
-            } else if (v.id == R.id.button_division) {
-                val intent = Intent(this, division::class.java)
-                intent.putExtra("VALUE7", value1)
-                intent.putExtra("VALUE8", value2)
-                startActivity(intent)
-
-            }
-        }
-
-        try {//ここどうしたら
         } catch (e: NumberFormatException) {
             Log.d("error","数字なし")
             textView.text = "数字を入れてね"
 
         } finally {
-            Log.d("zero", "数字なし")
+            Log.d("zero", "計算する")
+
+
+
+
+
+
+            if (v != null) {
+                if (v.id == R.id.button_plus) {
+                    val intent = Intent(this, Subapp::class.java)
+
+                    intent.putExtra("VALUE1", value1)
+                    intent.putExtra("VALUE2", value2)
+                    startActivity(intent)
+
+                } else if (v.id == R.id.button_minus) {
+                    val intent = Intent(this, minus::class.java)
+                    intent.putExtra("VALUE3", value1)
+                    intent.putExtra("VALUE4", value2)
+                    startActivity(intent)
+
+                } else if (v.id == R.id.button_double) {
+                    val intent = Intent(this, double1::class.java)
+                    intent.putExtra("VALUE5", value1)
+                    intent.putExtra("VALUE6", value2)
+                    startActivity(intent)
+
+                } else if (v.id == R.id.button_division) {
+                    val intent = Intent(this, division::class.java)
+                    intent.putExtra("VALUE7", value1)
+                    intent.putExtra("VALUE8", value2)
+                    startActivity(intent)
+
+                }
+            }
+
         }
 
 
         val intent = Intent(this, Subapp::class.java)
     }
-
-//       open fun showAlertDialog() {
-//            // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
-//            val alertDialogBuilder: AlertDialog.Builder = android.app.AlertDialog.Builder(this)
-//            alertDialogBuilder.setTitle("エラー")
-//            alertDialogBuilder.setMessage("数値以外のものが入力されています．")
-//
-//            // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
-//            alertDialogBuilder.setPositiveButton("OK",
-//                DialogInterface.OnClickListener { dialog, which -> })
-//
-//            // AlertDialogを作成して表示する
-//            val alertDialog: AlertDialog = alertDialogBuilder.create()
-//            alertDialog.show()
-//        }
-
 
     }
 
--- app/src/main/java/jp/techacademy/marika/calcapp/division.kt
+++ app/src/main/java/jp/techacademy/marika/calcapp/division.kt
@@ -12,21 +12,16 @@ class division : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_division)
 
-        val value1  = intent.getDoubleExtra("VALUE7", 0.0)
+        val value1 = intent.getDoubleExtra("VALUE7", 0.0)
         val value2 = intent.getDoubleExtra("VALUE8", 0.0)
 
-        
-        answer_division.text="${value1/value2}"
-
-
-
-
-
-
-
-
-
-
+        if (value2 == 0.0) {
+            answer_division.text = "０では割れないよ"
+        } else {
+            answer_division.text = "${value1 / value2}"
         }
+
+    }
 }
+
 
